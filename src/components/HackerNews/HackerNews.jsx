495b98d732a08db962fb68ce81825611648f9eea
mcasto-09 <mgcastorani@gmail.com>
2024-06-29 15:21:38 -0600

    step 4

--- src/components/HackerNews/HackerNews.jsx
+++ src/components/HackerNews/HackerNews.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 import Card from '../shared/Card/Card.jsx';
 import Loading from '../shared/Loading/Loading.jsx';
+import { requestArticles } from '../../reducers/hackerNewsReducer.js';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 export default function HackerNews() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const 
 
   const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
   return (
--- src/reducers/hackerNewsReducer.js
+++ src/reducers/hackerNewsReducer.js
@@ -1,10 +1,28 @@
 import { useReducer } from "react"
+import axios from "axios"
 
 let hackerInitialState = {
     loading: false,
-    artic
+    articles: []
+}
+ 
+const REQUEST_ARTICLES = 'REQUEST_ARTICLES'
+const PENDING = 'PENDING'
+
+export const requestArticles = async (dispatch) => {
+    dispatch({type: PENDING})
+    let articles = await axios.get('/api/hacker-news').then(res => res.data)
+    dispatch({ type: REQUEST_ARTICLES, payload: articles })
 }
 
-export default hackerInitialState ({
-    reducer: reducer
-})
+export default function hackerNewsReducer(state=hackerInitialState, action ) {
+    switch(action.type) {
+        case PENDING:
+            return {...state, loading: true}
+        case REQUEST_ARTICLES: 
+            return { loading: false, articles: action.payload }
+        default: 
+            return state
+    } 
+
+}
